ca4cd78d9fb0a1a8e798873f87b3fc15032440de
this pc <this pc@DESKTOP-2NC9HCH>
2021-05-15 19:21:17 +0530

    Implemented Recipe CRUD operations.

--- src/main/java/guru/springframework/spring5recipeapp/service/RecipeService.java
+++ src/main/java/guru/springframework/spring5recipeapp/service/RecipeService.java
@@ -19,4 +19,8 @@ public interface RecipeService {
 	Recipe findById(Long id);	
 	
 	RecipeCommand saveRecipeCommand(RecipeCommand command);
+	
+	RecipeCommand findCommandById(Long id);
+	
+	void deleteById(Long id);
 }
--- src/test/java/guru/springframework/spring5recipeapp/controller/RecipeControllerTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/controller/RecipeControllerTest.java
@@ -3,8 +3,12 @@
  */
 package guru.springframework.spring5recipeapp.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -14,9 +18,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import guru.springframework.spring5recipeapp.commands.RecipeCommand;
 import guru.springframework.spring5recipeapp.model.Recipe;
 import guru.springframework.spring5recipeapp.service.RecipeService;
 
@@ -29,27 +35,62 @@ public class RecipeControllerTest {
 
 	@Mock
 	private RecipeService recipeService;
-	
+
 	@InjectMocks
 	private RecipeController recipeController;
-	
+
 	MockMvc mockMvc;
-	
+
 	Recipe recipe;
-	
+
 	@BeforeEach
 	public void setup() {
 		recipe = Recipe.builder().id(1L).build();
-		
+
 		mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
 	}
-	
+
 	@Test
 	public void showDetails() throws Exception {
 		when(recipeService.findById(1L)).thenReturn(recipe);
-		
-		mockMvc.perform(get("/recipe/show/"+1L))
-			.andExpect(status().isOk())
-			.andExpect(view().name("recipe/show"));
+
+		mockMvc.perform(get("/recipe/" + 1L+"/show")).andExpect(status().isOk()).andExpect(view().name("recipe/show"));
+	}
+
+	@Test
+	public void testGetNewRecipeForm() throws Exception {
+		RecipeCommand command = new RecipeCommand();
+
+		mockMvc.perform(get("/recipe/create")).andExpect(status().isOk()).andExpect(view().name("recipe/recipeform"))
+				.andExpect(model().attributeExists("recipe"));
+	}
+
+	@Test
+	public void testPostNewRecipeForm() throws Exception {
+		RecipeCommand command = new RecipeCommand();
+		command.setId(2L);
+
+		when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+		mockMvc.perform(post("/recipe").contentType(MediaType.APPLICATION_FORM_URLENCODED)
+//	                .param("id", "")
+//	                .param("description", "some string")
+		).andExpect(status().is3xxRedirection()).andExpect(view().name("redirect:/recipe/"+command.getId()+"/show"));
+	}
+
+	@Test
+	public void testGetUpdateView() throws Exception {
+		RecipeCommand command = new RecipeCommand();
+		command.setId(2L);
+
+		when(recipeService.findCommandById(anyLong())).thenReturn(command);
+
+		mockMvc.perform(get("/recipe/1/update")).andExpect(status().isOk()).andExpect(view().name("recipe/recipeform"))
+				.andExpect(model().attributeExists("recipe"));
+	}
+	
+	@Test
+	public void testGetDeleteView() throws Exception {
+		mockMvc.perform(get("/recipe/1/delete")).andExpect(status().is3xxRedirection()).andExpect(view().name("redirect:/"));
 	}
 }
